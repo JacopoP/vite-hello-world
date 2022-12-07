c518038a9a7126c6f15234d446f523d362171c9f
Jac22 <jacopo.prudentino@gmail.com>
2022-12-07 17:49:24 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            msg: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+
+</style>
